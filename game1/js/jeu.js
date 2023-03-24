9866747b7086cd2a98a2e88f391a7900cad5beb7
William <128689649+WilliammCorreia@users.noreply.github.com>
2023-03-24 19:47:42 +0100

    Meilleur Score

--- game1/js/jeu.js
+++ game1/js/jeu.js
@@ -4,11 +4,18 @@ console.log('Jeu démarré');
 let box = document.querySelector('.box');
 let click = 0;
 let scoreElement = document.querySelector('#score');
+let highest_score = 0;
+let highest_scoreElement = document.querySelector('#highest_score');
 
 let chrono = 10;
 let chronoElement =  document.querySelector('#chrono');
 chronoElement.innerHTML = chrono;
 
+
+// Pour afficher le meilleur score
+highest_score = localStorage.getItem("highest_score");
+// innerHTML C'est pour écrire dans le HTML
+highest_scoreElement.innerHTML = highest_score;
 
 
 box.addEventListener("click", () => {
@@ -18,6 +25,16 @@ if( chrono != 0 )
     click += 1;
     // innerHTML C'est pour écrire dans le HTML
     scoreElement.innerHTML = click;
+
+    // Pour attribuer le meilleur score
+    if( click > highest_score )
+    {
+        highest_score = click;
+        localStorage.setItem("highest_score",highest_score);
+        // innerHTML C'est pour écrire dans le HTML
+        highest_scoreElement.innerHTML = highest_score;
+    }
+
     // Génère un nombre aléatoire qui sera stocké dans la variable top
     // window.innerHeight C'est pour récupérer la taille de l'écran
     let top = Math.floor(Math.random() * window.innerHeight);
